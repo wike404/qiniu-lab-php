0bb7c7a35c7bb447334e94ca2b0200309574a4eb
Jemy <jemygraw@gmail.com>
2014-12-31 14:14:44 +0800

    Add simple file upload example.

---
+++ demos/upload/simple_upload_use_return_body.php
@@ -0,0 +1,40 @@
+<?php
+require_once("../../qiniu_config.php");
+require_once("../../lib/qiniu/io.php");
+require_once("../../lib/qiniu/rs.php");
+require_once("print_result.php");
+
+//使用ReturnBody自定义返回内容
+$putPolicy = new Qiniu_RS_PutPolicy($Qiniu_Public_Bucket);
+//以json格式组织回复内容，支持魔法变量和扩展变量。
+//文档：http://developer.qiniu.com/docs/v6/api/overview/up/response/vars.html
+$returnBody = array(
+    "bucket" => "$(bucket)",
+    "fsize" => "$(fsize)",
+    "fname" => "$(fname)",
+    "etag" => "$(etag)",
+    "key" => "$(key)",
+    "exParam1" => "$(x:exParam1)",
+    "exParam2" => "$(x:exParam2)",
+    "exParam3" => "$(x:exParam3)",
+    "exParam4" => "$(exParam4)", //这个扩展参数没有以x:开头，会被忽略
+);
+$putPolicy->ReturnBody = json_encode($returnBody);
+$auth = new Qiniu_Mac($Qiniu_AccessKey, $Qiniu_SecretKey);
+$upToken = $putPolicy->Token($auth);
+$key = "hello_qiniu_logo_20141231.png";
+$localFile = "/Users/jemy/Documents/qiniu.png";
+$putExtra = new Qiniu_PutExtra();
+$putExtra->MimeType = "image/png";
+$putExtra->CheckCrc = TRUE;
+$putExtra->Crc32 = crc32(file_get_contents($localFile));
+//扩展参数必须以x:开头，否则会被忽略
+$params = array(
+    "x:exParam1" => "hello",
+    "x:exParam2" => "qiniu",
+    "x:exParam3" => "cloud",
+    "exParam4" => "storage",
+);
+$putExtra->Params = $params;
+list($ret, $err) = Qiniu_PutFile($upToken, $key, $localFile, $putExtra);
+QiniuLab_PrintUploadResult($ret, $err);
--- demos/upload/simple_upload_without_key.php
+++ demos/upload/simple_upload_without_key.php
@@ -1,119 +1,56 @@
 <?php
-$PAGE_TITLE = "简单文件上传－不指定上传文件的key";
-?>
-<?php
-require ("../../header.php");
-?>
-<?php
-require ("../../qiniu_config.php");
-?>
-<?php
-//create token
-require_once ("../../lib/qiniu/rs.php");
+require_once("../../qiniu_config.php");
+require_once("../../lib/qiniu/io.php");
+require_once("../../lib/qiniu/rs.php");
+require_once("print_result.php");
+
 Qiniu_SetKeys($Qiniu_AccessKey, $Qiniu_SecretKey);
 $putPolicy = new Qiniu_RS_PutPolicy($Qiniu_Public_Bucket);
-$token = $putPolicy -> Token(null);
-?>
-<p class="title">
-	简单上传-不指定上传文件key
-</p>
 
-<div class="panel panel-default">
-	<div class="panel-heading">
-		目的
-	</div>
-	<div class="panel-body">
-		该实验的目的是迅速体验一下七牛的文件上传服务，你可以上传任何一个文件到七牛的服务器，而不需要指定一个名字。
-		七牛服务器会根据文件的内容来计算这个文件标识符来作为文件的名字（即key）。
-		计算文件标识符的算法参考<a href="https://github.com/qiniu/qetag">etag</a>。
-	</div>
-</div>
-<div class="panel panel-default">
-	<div class="panel-heading">
-		实验
-	</div>
-	<div class="panel-body">
-		<form method="post" action="http://upload.qiniu.com" enctype="multipart/form-data" role="form">
-			<div class="form-group">
-				<div class="input-group">
-					<span class="input-group-addon">Token</span>
-					<input name="token" type="text" value="<?php echo $token; ?>"
-					class="form-control" readonly="true"
-					/>
-				</div>
-			</div>
-			<div class="form-group">
-				<input name="file" type="file"/>
-			</div>
-			<div class="form-group">
-				<input type="submit" value="Upload" class="btn btn-primary"/>
-			</div>
-		</form>
-	</div>
-</div>
-<div class="panel panel-default">
-	<div class="panel-heading">
-		代码
-	</div>
-	<div class="panel-body">
-		<div id="code-tabs">
-			<ul class="nav nav-tabs" role="tablist">
-				<li role="presentation">
-					<a href="#php-code">PHP</a>
-				</li>
-				<li role="presentation">
-					<a href="#java-code">Java</a>
-				</li>
-			</ul>
-			<div id="php-code">
-				<?php
-				include ("../code_snippet/php/simple_upload_with_key_token_php.html");
-				?>
-			</div>
-			<div id="java-code">
-				<?php
-				include ("../code_snippet/java/simple_upload_with_key_token_java.html");
-				?>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="panel panel-default">
-	<div class="panel-heading">
-		结果
-	</div>
-	<div class="panel-body">
-		<div class="tip">
-			<p>
-				<strong>上传成功结果：</strong>
-			</p>
-			<code>
-				{"hash":"FoaI_ZzokcGpZA48PVJPFNJvLoNc","key":"FoaI_ZzokcGpZA48PVJPFNJvLoNc"}
-			</code>
+//无key文件上传
+$upToken = $putPolicy->Token(null);
+$localFile = "/Users/jemy/Documents/qiniu.jpg";
+list($ret, $err) = Qiniu_PutFile($upToken, null, $localFile, null);
+QiniuLab_PrintUploadResult($ret, $err);
 
-		</div>
-		<div class="tip">
-			<p>
-				<strong>上传失败结果：（未指定文件错误）</strong>
-			</p>
-			<code>
-				{"error":"file is not specified in multipart"}
-			</code>
-		</div>
-	</div>
-</div>
+//无Key数据上传
+$upToken = $putPolicy->Token(null);
+$localData = "hello world, 七牛云存储";
+list($ret, $err) = Qiniu_Put($upToken, null, $localData, null);
+QiniuLab_PrintUploadResult($ret, $err);
 
-<div class="panel panel-default">
-	<div class="panel-heading">
-		备注
-	</div>
-	<div class="panel-body">
-		这种是最简单的上传方式，只指定了上传文件和需要的token。
-		而七牛的后台会根据文件内容的<a href="https://github.com/qiniu/qetag">etag</a>来为文件命名。
-		如果重复上传相同的文件内容，则七牛也只保留一个文件。
-		默认上传完之后，七牛服务器返回的是Content-Type为application/json的json格式字符串。
-	</div>
-</div>
-<?php
-require ("../../footer.php");
-?>
+//手动设置mimeType
+$upToken = $putPolicy->Token(null);
+$localData = "hello qiniu, 七牛云存储";
+$putExtra = new Qiniu_PutExtra();
+$putExtra->MimeType = "text/plain";
+list($ret, $err) = Qiniu_Put($upToken, null, $localData, $putExtra);
+QiniuLab_PrintUploadResult($ret, $err);
+
+//带Crc32检测
+$upToken = $putPolicy->Token(null);
+$localData = "qiniu cloud, 七牛云存储";
+$putExtra = new Qiniu_PutExtra();
+$putExtra->MimeType = "text/plain";
+$putExtra->CheckCrc = TRUE;
+$putExtra->Crc32 = crc32($localData);
+list($ret, $err) = Qiniu_Put($upToken, null, $localData, $putExtra);
+QiniuLab_PrintUploadResult($ret, $err);
+
+//带扩展参数
+$upToken = $putPolicy->Token(null);
+$localData = "qiniu cloud storage, 七牛云存储";
+$putExtra = new Qiniu_PutExtra();
+$putExtra->MimeType = "text/plain";
+$putExtra->CheckCrc = TRUE;
+$putExtra->Crc32 = crc32($localData);
+//扩展参数必须以x:开头，否则会被忽略
+$params = array(
+    "x:exParam1" => "hello",
+    "x:exParam2" => "qiniu",
+    "x:exParam3" => "cloud",
+    "exParam4" => "storage",
+);
+$putExtra->Params = $params;
+list($ret, $err) = Qiniu_Put($upToken, null, $localData, $putExtra);
+QiniuLab_PrintUploadResult($ret, $err);
