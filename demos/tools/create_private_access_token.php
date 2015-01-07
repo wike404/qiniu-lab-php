9a88556d99981c201cc6d722531c52a4c99cd184
Jemy <jemygraw@gmail.com>
2015-01-07 14:23:52 +0800

    Update create private access token example. remove the qiniudn part.

--- demos/tools/create_private_access_token.php
+++ demos/tools/create_private_access_token.php
@@ -7,19 +7,6 @@ require_once ("../../lib/qiniu/auth_digest.php");
 
 <?php
 date_default_timezone_set('Asia/Shanghai');
-if (isset($_POST["bucket"]) && isset($_POST["key"])) {
-	$bucket = $_POST["bucket"];
-	$key = $_POST["key"];
-	$expire = $_POST["expire"];
-	if (!empty($bucket) && !empty($key) && !empty($expire)) {
-		$timestamp = strtotime($expire);
-		$srcUrl = sprintf("http://%s.qiniudn.com/%s", $bucket, urlencode($key));
-		$srcUrl = sprintf("%s?e=%d", $srcUrl, $timestamp);
-		$mac = new Qiniu_Mac($Qiniu_AccessKey, $Qiniu_SecretKey);
-		$accessToken = $mac -> Sign($srcUrl);
-		$newUrl = sprintf("%s&token=%s", $srcUrl, $accessToken);
-	}
-}
 if (isset($_POST["resSrcUrl"]) && isset($_POST["resExpire"])) {
 	$resSrcUrl = $_POST["resSrcUrl"];
 	$resExpire = $_POST["resExpire"];
@@ -44,60 +31,14 @@ if (isset($_POST["resSrcUrl"]) && isset($_POST["resExpire"])) {
 		目的
 	</div>
 	<div class="panel-body">
-		该实验演示如何创建私有空间中文件访问所需要的Token，具体参考<a href="http://developer.qiniu.com/docs/v6/api/reference/security/download-token.html">文档</a>。
-		该例子使用
-		<code>
-			qiniudn.com
-		</code>
-		的默认域名，如果你需要其他的域名只需要改一下域名即可。
+		该实验演示如何创建私有空间中文件访问所需要的Token，具体参考<a href="http://developer.qiniu.com/docs/v6/api/reference/security/download-token.html">文档</a>
 	</div>
 </div>
 <div class="row">
 	<div class="col-md-6">
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				实验1 - 常规私有资源访问
-			</div>
-			<div class="panel-body">
-				<form method="post" action="" role="form">
-					<div class="form-group">
-						<input name="bucket" value="<?php
-						if (isset($bucket)) {echo $bucket;
-						}
-					?>" placeholder="请指定空间名称" class="form-control"/>
-					</div>
-					<div class="form-group">
-						<input name="key" value="<?php
-						if (isset($key)) {echo $key;
-						}
-					?>" placeholder="请指定文件的key" class="form-control"/>
-					</div>
-					<div class="form-group">
-						<input name="expire" value="<?php if(isset($expire)) {echo $expire;}?>" placeholder="请指定过期时间, 格式yyyy-MM-dd HH:mm:ss" class="form-control"/>
-					</div>
-					<div class="form-group">
-						<input type="submit" value="生成私有资源访问Token" class="btn btn-info"/>
-					</div>
-				</form>
-				<div class="form-group">
-					<input name="key" value="<?php
-					if (isset($accessToken)) {echo $accessToken;
-					}
-					?>" placeholder="生成的Token" class="form-control" readonly="true"/>
-				</div>
-				<div class="form-group">
-					<input name="key" value="<?php
-					if (isset($newUrl)) {echo $newUrl;
-					}
-					?>" placeholder="生成的私有资源访问链接" class="form-control" readonly="true"/>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="col-md-6">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				实验2 - 带实时fop的私有链接访问
+				实验
 			</div>
 			<div class="panel-body">
 				<form method="post" action="" role="form">
@@ -120,6 +61,18 @@ if (isset($_POST["resSrcUrl"]) && isset($_POST["resExpire"])) {
 			</div>
 		</div>
 	</div>
+    <div class="col-md-6">
+        <p>
+            步骤:<br/>
+            <ol>
+                <li>找到空间对应的下载域名，比如我的私有空间if-pri对应的下载域名是http://7qnctm.com1.z0.glb.clouddn.com</li>
+                <li>找到文件名作为key，附加在http://7qnctm.com1.z0.glb.clouddn.com后面，构成http://7qnctm.com1.z0.glb.clouddn.com/key</li>
+                <li>将上面的资源链接填入“访问链接”输入框</li>
+                <li>如果链接是带fop操作的，那么在http://7qnctm.com1.z0.glb.clouddn.com/key后面，先接上fop相关指令参数，比如http://7qnctm.com1.z0.glb.clouddn.com/key?imageView2/0/1/w/100，再填入“访问链接”输入框</li>
+                <li>输入该完整访问链接的过期时间</li>
+            </ol>
+        </p>
+    </div>
 </div>
 
 <?php
